cd8d4f498ae05be796243f770042488773d29a1a
MaggieChafee <maggie.chafee@gmail.com>
2024-04-12 14:40:41 -0500

    basic card view completed, logo added

---
+++ components/cards/orderCard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import PropTypes from 'prop-types';
+
+function OrderCard({ orderObj }) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{orderObj.name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{orderObj.orderOpen ? 'OPEN' : 'CLOSED'}</Card.Subtitle>
+        <Card.Text>
+          {orderObj.phoneNumber}
+          {orderObj.email}
+          {orderObj.orderType}
+        </Card.Text>
+        <Card.Link href="#">View</Card.Link>
+        <Card.Link href="#">Edit</Card.Link>
+        <Card.Link href="#">Delete</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+OrderCard.propTypes = {
+  orderObj: PropTypes.shape({
+    name: PropTypes.string,
+    phoneNumber: PropTypes.string,
+    email: PropTypes.string,
+    orderType: PropTypes.string,
+    orderOpen: PropTypes.bool,
+  }).isRequired,
+};
+
+export default OrderCard;
--- pages/orders/allOrders.js
+++ pages/orders/allOrders.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllOrders } from '../../api/ordersApi';
+import OrderCard from '../../components/cards/orderCard';
 
-export default function ViewAllOrders() {
+function ViewAllOrders() {
+  const [orders, setOrders] = useState([]);
+  const getOrders = () => {
+    getAllOrders().then(setOrders);
+  };
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
   return (
     <div>
-      <h1>View All Orders</h1>
+      <h1>All Orders</h1>
+      <div>
+        {orders.map((order) => (
+          <OrderCard key={order.id} orderObj={order} />
+        ))}
+      </div>
     </div>
   );
 }
+
+export default ViewAllOrders;
